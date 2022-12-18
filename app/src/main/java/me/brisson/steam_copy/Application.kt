8ba2c589dd272b7a2b887a85bbd176c209bfaa10
guiBrisson <54915600+guiBrisson@users.noreply.github.com>
2022-12-18 11:29:58 -0300

    Merge pull request #1
    * adding hilt and introducing mvvm with uistate to project

---
+++ app/src/main/java/me/brisson/steam_copy/Application.kt
@@ -0,0 +1,7 @@
+package me.brisson.steam_copy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application()
--- app/src/main/java/me/brisson/steam_copy/MainActivity.kt
+++ app/src/main/java/me/brisson/steam_copy/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
+import dagger.hilt.android.AndroidEntryPoint
 import me.brisson.steam_copy.ui.theme.SteamCopyTheme
 
 @ExperimentalAnimationApi
 @ExperimentalMaterial3Api
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
